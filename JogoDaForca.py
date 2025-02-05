0bea0e5e11732dbb6c32bc551b0a5950917619ab
Luizrattacaso <167656308+Luizrattacaso@users.noreply.github.com>
2025-02-05 10:53:50 -0300

    Update JogoDaForca.py
    adição de sistema de pontuação ao jogo

--- JogoDaForca.py
+++ JogoDaForca.py
@@ -10,6 +10,8 @@ from kivy.uix.boxlayout import BoxLayout
 from texto import palavras_com_dicas
 import random
 
+Max_tentativas = 6
+
 class ForcaApp(App):
     def build(self):
         self.iniciar_jogo()
@@ -18,8 +20,12 @@ class ForcaApp(App):
         layout.add_widget(self.label_palavra)
 
         #erros
-        self.label_erros = Label(text=f"Tentativas restantes: {6 - self.erros}", font_size=20)
+        self.label_erros = Label(text=f"Tentativas restantes: {Max_tentativas - self.erros}", font_size=20)
         layout.add_widget(self.label_erros)
+
+        #pontuação
+        self.label_pontuacao = Label(text=f"Pontuação: {self.pontuacao}", font_size=20)
+        layout.add_widget(self.label_pontuacao)
 
         #letra
         self.text_input = TextInput(hint_text="Digite uma letra", font_size=30, multiline=False)
@@ -30,7 +36,6 @@ class ForcaApp(App):
         botao_layout = BoxLayout(orientation='horizontal', size_hint=(None, None), size=(400, 50), spacing=10)
         botao_layout.add_widget(self.botao_dica)
         botao_layout.add_widget(self.botao_reiniciar)
-
         layout.add_widget(botao_layout)
 
         return layout
@@ -43,8 +48,13 @@ class ForcaApp(App):
         self.n = len(self.palavra)
         self.erros = 0
         self.chutes = ""
+        self.pontuacao = 0
         self.exibicao = "_ " * self.n  #exibição
         self.acerto = False
+
+        if not palavras_com_dicas:
+            self.popup_mensagem("Erro: Nenhuma palavra disponível.")
+            return
 
         #botões
         self.botao_dica = Button(text="Mostrar Dica", size_hint=(None, None), size=(200, 50))
@@ -57,11 +67,31 @@ class ForcaApp(App):
         letra = self.text_input.text.lower()
 
         # Verificando se a entrada é uma letra válida
-        if len(letra) != 1 or not letra.isalpha():
+        if len(letra) > 1:
+            if letra == self.palavra:
+                self.exibicao = f"Parabéns, você acertou a palavra: {self.palavra}"
+                self.label_palavra.text = self.exibicao
+                if len(self.chutes) == 0:
+                    self.pontuacao +=1500
+                    self.label_pontuacao.text = f"Pontuação: {self.pontuacao}"
+                elif len(self.chutes) <= 3:
+                    self.pontuacao +=1000
+                    self.label_pontuacao.text = f"Pontuação: {self.pontuacao}"
+                else:
+                    self.pontuacao += 800
+                    self.label_pontuacao.text = f"Pontuação: {self.pontuacao}"
+                self.popup_mensagem("Você venceu!")
+                self.desabilitar_input()
+            else:
+                self.erros += 1
+                self.label_erros.text = f"Tentativas restantes: {Max_tentativas - self.erros}"
+                self.popup_mensagem(f"Errou! A palavra não é {letra}")
+
+        if (len(letra) != 1 and letra != self.palavra) or not letra.isalpha():
             self.popup_mensagem("Por favor, digite uma letra válida.")
             self.text_input.text = "" 
             return
-
+        
         if letra in self.chutes:
             self.popup_mensagem("Você já tentou essa letra!")
             self.text_input.text = ""  
@@ -71,6 +101,8 @@ class ForcaApp(App):
 
         if letra in self.palavra:
             nova = ""
+            self.pontuacao += 50
+            self.label_pontuacao.text = (f"Pontuação: {self.pontuacao}")
             for i in range(self.n):
                 if self.palavra[i] in self.chutes:
                     nova += self.palavra[i] + " "
@@ -80,15 +112,17 @@ class ForcaApp(App):
             self.label_palavra.text = self.exibicao
         else:
             self.erros += 1
-            self.label_erros.text = f"Tentativas restantes: {6 - self.erros}"
+            self.pontuacao -= 30
+            self.label_pontuacao.text = (f"Pontuação: {self.pontuacao}")
+            self.label_erros.text = f"Tentativas restantes: {Max_tentativas - self.erros}"
 
-        if "_ " not in self.exibicao:  #ganhou
+        if "_ " not in self.exibicao:  #ganha
             self.exibicao = f"Parabéns, você acertou a palavra: {self.palavra}"
             self.label_palavra.text = self.exibicao
             self.popup_mensagem("Você venceu!")
             self.desabilitar_input()
 
-        elif self.erros >= 6: #perdeu
+        elif self.erros >= Max_tentativas: #perde
             self.exibicao = (f"Você perdeu! A palavra era: {self.palavra}")
             self.label_palavra.text = self.exibicao
             self.popup_mensagem("Você perdeu!")
@@ -99,8 +133,10 @@ class ForcaApp(App):
     def reiniciar(self, instance):
         ("""Reinicia o jogo.""")
         self.iniciar_jogo()
+        self.pontuacao = 0
         self.label_palavra.text = self.exibicao
-        self.label_erros.text = (f"Tentativas restantes: {6 - self.erros}")
+        self.label_erros.text = (f"Tentativas restantes: {Max_tentativas - self.erros}")
+        self.label_pontuacao.text = (f"Pontuação: {self.pontuacao}")
         self.text_input.text = ""
         self.text_input.disabled = False
 
@@ -121,6 +157,9 @@ class ForcaApp(App):
     def desabilitar_input(self):
         ("""Desabilita a entrada após o fim do jogo.""")
         self.text_input.disabled = True
+        self.label_palavra.color = (0, 1, 0, 1)  # Verde para vitória
+        if self.erros >= Max_tentativas:
+            self.label_palavra.color = (1, 0, 0, 1)  # Vermelho para derrota
 
 if __name__ == '__main__':
     ForcaApp().run()
